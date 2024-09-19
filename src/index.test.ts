2e7d3e1071e9a0f658d39e8d69ecc6f6a0dbe8d6
Víctor Reyes <unhill@gmail.com>
2024-09-19 23:57:17 +0200

    Add test for empty name input and update function to handle it

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,10 @@
+import { throws } from "node:assert";
 import { deepEqual } from "node:assert/strict";
 import { test } from "node:test";
 
 function greet(name: string) {
+  if (name.trim().length === 0) throw new Error("The name should not be empty");
+
   return { message: `Welcome, ${name}!` };
 }
 
@@ -9,4 +12,11 @@ test("should greet person", () => {
   const result = greet("Alek");
 
   deepEqual(result, { message: "Welcome, Alek!" });
+});
+
+test("should throw, if the name is empty", () => {
+  const errorMessage = /The name should not be empty$/;
+  throws(() => greet(""), errorMessage);
+  throws(() => greet(" "), errorMessage);
+  throws(() => greet("  "), errorMessage);
 });
